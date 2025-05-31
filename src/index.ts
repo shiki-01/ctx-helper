037266a7c0081738f44f7bd02634b7bbe143b587
shiki <0110shiki@gmail.com>
2025-05-31 14:27:11 +0900

    プロセス分離

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { ipcManager } from './common/ipcManager/index.js';
-import { apiManager } from './preload/index.js';
+import { mainAPIManager } from './main/index.js';
+import { rendererAPIManager } from './preload/index.js';
 import type { APIRecord, Status, APISchema, AsyncFunction, RecursiveAPI, RecursiveListener, StatusSchema } from './types/index.js';
 
 export {
     ipcManager,
-    apiManager,
+    mainAPIManager,
+    rendererAPIManager
 }
 export type {
     APIRecord,
